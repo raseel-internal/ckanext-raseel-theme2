cad9539e64e267598844bfc6102e5d3f61b83a15
Houssem Badri <hbadri@mda.gov.sa>
2022-11-07 11:14:21 +0300

    fix

--- ckanext/stanti_theme/plugin.py
+++ ckanext/stanti_theme/plugin.py
@@ -11,4 +11,4 @@ class StantiThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'stanti_theme')
+        toolkit.add_resource('fanstatic', 'raseel_theme')
