77fb375095013c57683a6e8543a8ea6b2811f64b
meriam <mba@atit-sa.com>
2022-12-01 14:06:00 +0100

    remove views folder

--- ckanext/raseel_theme/plugin.py
+++ ckanext/raseel_theme/plugin.py
@@ -4,7 +4,6 @@ import ckan.plugins.toolkit as toolkit
 
 class RaseelThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
@@ -12,15 +11,4 @@ class RaseelThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'raseel_theme')  
-    
-    def get_blueprint(self):
-        """
-        CKAN uses Flask Blueprints in the /ckan/views dir for user and dashboard
-        Here we override some routes to redirect unauthenticated users to the logout page, and only redirect the
-        user to the `came_from` URL if they are logged in.
-        :return:
-        """
-        from .views import user
-        blueprints = user.get_blueprints()
-        return blueprints
     
